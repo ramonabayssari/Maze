6a52241b708f09fe17bc615740dcfcd70fa6e0d5
ramonabayssari <102956605+ramonabayssari@users.noreply.github.com>
2022-09-18 11:44:58 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 let isStart = false;
-    
+let isInGame = false;    
+
  window.onload = function () {
         const start = document.getElementById("start");
         const end = document.getElementById("end");
@@ -11,11 +12,21 @@ let isStart = false;
         start.addEventListener("mouseover", function () {
             stat.textContent = "Begin by moving your mouse over the S";
             isStart = true;
-            isInMap = true;
+            isInGame= true;
             for (let i = 0; i < boundary.length; i++) { boundary[i].style.backgroundColor = "#eeeeee"; }
+        })//each time we repress S the boundaries go back to their default color 
     
-        })
-    
+
+        //whenever the mouse touches (mouseover) any boundary, their color turn red and the player looses the game 
+        for (var i = 0; i < boundary.length; i++)
+            boundary[i].addEventListener("mouseover", function () {
+                    if (isStart) {
+                        for (let i = 0; i < boundary.length; i++) { boundary[i].style.backgroundColor = "#FF0000"; }
+                        stat.textContent = "You hit the boundary, lost the game!";
+                        isStart = false;
+                    }
+                });
+
     
         //arrives to destination and ends the game
         end.addEventListener("mouseover", function () {
