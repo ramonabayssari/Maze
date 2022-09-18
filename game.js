ab7386fce24b29751c49c3321bb4b8ba1997cfb3
ramonabayssari <102956605+ramonabayssari@users.noreply.github.com>
2022-09-18 14:04:34 +0300

    Update game.js

--- game.js
+++ game.js
@@ -8,6 +8,9 @@ let isInGame = false;
         const game =  document.getElementById("game");
         const stat = document.getElementById("status");
     
+
+        
+
         //press S to start 
         start.addEventListener("mouseover", function () {
             stat.textContent = "Begin by moving your mouse over the S";
@@ -17,22 +20,38 @@ let isInGame = false;
         })//each time we repress S the boundaries go back to their default color 
     
 
+
+
         //whenever the mouse touches (mouseover) any boundary, their color turn red and the player looses the game 
         for (var i = 0; i < boundary.length; i++)
             boundary[i].addEventListener("mouseover", function () {
+                     stat.textContent = "You hit the boundary, lost the game!";
                     if (isStart) {
-                        for (let i = 0; i < boundary.length; i++) { boundary[i].style.backgroundColor = "#FF0000"; }
-                        stat.textContent = "You hit the boundary, lost the game!";
-                        isStart = false;
+                        for (let i = 0; i < boundary.length; i++) { boundary[i].style.backgroundColor = "#FF0000"; }                       
                     }
+                    isStart = false;//to end and restart the game
                 });
 
-    
+
+            
+        
+        //to know when the mouse gets out (mouseleave so when the mouse leave the child elements it wont be 
+        //triggered) of the game div to cheat 
+        game.addEventListener("mouseleave", function () {
+            isInGame = false;
+            stat.textContent="Nah bruh you can not cheat, LOSER!";
+            for (let i = 0; i < boundary.length; i++) { boundary[i].style.backgroundColor = "#FF0000"; }  
+            isStart=false;                     
+        });
+
+
+
+
         //arrives to destination and ends the game
         end.addEventListener("mouseover", function () {
-            if (isStart) {
+            if(isStart==true){
                 stat.textContent = "Congratulation! You win the game!"; 
-            }
+                for (let i = 0; i < boundary.length; i++) { boundary[i].style.backgroundColor = "green"; }}
             isStart = false;//to end and restart the game
         });
     
